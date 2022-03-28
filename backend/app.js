cc47e89efd6f85cb45b6afb3c8ff944cf3efd9e0
DXZ <774740493@qq.com>
2022-03-28 18:36:10 -0400

    added API functionalities that  get user info and update user info

--- backend/app.js
+++ backend/app.js
@@ -15,14 +15,19 @@ const productApi = "/api/product";
 const getAllUsers = require("./src/routes/getAllUsers");
 const createNewUser = require("./src/routes/createNewUser");
 const userLogin = require("./src/routes/userAuthentication");
+const getUserProfile = require("./src/routes/getUserProfile");
+const updateUserProfile = require("./src/routes/updateUserProfile");
 
 const getAllProducts = require("./src/routes/getAllProducts");
 const createNewProduct = require("./src/routes/createNewProduct");
 
 // user api
 app.get(`${userApi}/fetchAllUsers`, getAllUsers);
+app.get(`${userApi}/getUserProfile/:userID`, getUserProfile);
+
 app.post(`${userApi}/signUp`, createNewUser);
 app.post(`${userApi}/signIn`, userLogin);
+app.patch(`${userApi}/updateUserProfile/:userID`, updateUserProfile);
 
 // product api
 app.get(`${productApi}/fetchAllProducts`, getAllProducts);
